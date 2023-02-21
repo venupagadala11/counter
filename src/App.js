6091026fc5e00afbea633bab1e577571cb579f55
venupagadala11 <venugopal@divami.com>
2023-02-21 20:14:12 +0530

    update

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import './App.css';
+
+
+function App() {
+
+  const [count, setCount]=useState(0)
+
+    const increment = ()=>
+    {
+        setCount(count+1);
+        console.log(count)
+    } 
+
+    const decrement = ()=>
+    {
+        setCount(count-1);
+        console.log(count)
+    } 
+
+  return (
+    <div className="App">
+      <div class="container">
+        <button onClick={increment}>Increment</button>
+        <h1>{count}</h1>
+        <button onClick={decrement}>Decrementr</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
